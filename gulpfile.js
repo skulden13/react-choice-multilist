71d3ccb713d7c68b6a5d081384a91b843c00af66
denis.skulimovskyy <skulden13@gmail.com>
2016-03-12 19:42:50 +0300

    gulp: use sass, add sass compiler, copy fonts task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,19 @@ var runSequence = require('run-sequence');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 var port = process.env.port || 3031;
+
+gulp.task('copy-fonts', function () {
+  return gulp.src('./node_modules/font-awesome/fonts/**.*')
+    .pipe(gulp.dest('./app/dist/fonts/'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./app/src/css/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/dist/css/'));
+});
 
 gulp.task('browserify', function() {
 	gulp.src('./app/src/js/App.jsx')
@@ -31,6 +43,11 @@ gulp.task('connect', function() {
 	});
 });
 
+gulp.task('css', function() {
+	gulp.src('./app/*.css')
+		.pipe(connect.reload());
+});
+
 gulp.task('js', function() {
 	gulp.src('./app/dist/**/*.js')
 		.pipe(connect.reload());
@@ -42,15 +59,16 @@ gulp.task('html', function() {
 });
 
 gulp.task('watch', function() {
+	gulp.watch('app/dist/css/*.css', ['css']);
 	gulp.watch('app/dist/js/*.js', ['js']);
 	gulp.watch('app/index.html', ['html']);
 	gulp.watch('app/src/js/**/*.jsx', ['browserify']);
 });
 
 gulp.task('default', function(){
-	runSequence('browserify');
+	runSequence('browserify', 'copy-fonts', 'sass');
 });
 
 gulp.task('serve', function(){
-	runSequence('browserify', 'connect', 'open', 'watch');
+	runSequence('browserify', 'copy-fonts', 'sass', 'connect', 'open', 'watch');
 });
